18af1bc0fde9eb2d5534a74d40f9303e31bae874
devAmanYadav <ay577152@gmail.com>
2026-04-07 18:32:10 +0530

    final rename सही

--- src/main/java/com/rdsystem/controller/RdPassbookController.java
+++ src/main/java/com/rdsystem/controller/RdPassbookController.java
@@ -23,7 +23,7 @@ import com.rdsystem.services.PassbookService;
 @CrossOrigin(origins = "http://localhost:5174")
 @RestController
 @RequestMapping("/api")
-public class temp {
+public class RdPassbookController {
 
 	@Autowired
 	private rdpassbookRepo prepo;
